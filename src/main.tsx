a825dd9dfffa64287e96a0d5c3896d35c3006532
Wishr <jeff@wishr.com>
2025-01-28 14:41:51 +0000

    Commit from Bolt to GitHub

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './index.css';
 
@@ -11,7 +10,6 @@ createRoot(document.getElementById('root')!).render(
     <HelmetProvider>
       <BrowserRouter>
         <App />
-        <Analytics />
       </BrowserRouter>
     </HelmetProvider>
   </StrictMode>
